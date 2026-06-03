408ae24ac0cc64a462b0930d69960c0f89179c55
palani-appan <165807237+palani-appan@users.noreply.github.com>
2026-06-03 13:27:05 -0400

    Define variables for Google Storage Bucket
    Added variables for bucket configuration in GCP.

--- variables.tf
+++ variables.tf
@@ -1,1 +1,29 @@
+variable "bucket_name" {
+  type        = string
+  description = "The name of the Google Storage Bucket."
+ 
+}
 
+variable "project_id" {
+  type        = string
+  description = "The ID of the GCP project where the bucket will be created."
+  
+}
+
+variable "location" {
+  type        = string
+  description = "The multi-region, region, or dual-region location for the bucket."
+
+}
+
+variable "force_destroy" {
+  type        = bool
+  description = "When deleting a bucket, this boolean option will delete all contained objects."
+
+}
+
+variable "public_access_prevention" {
+  type        = string
+  description = "Prevents public access to a bucket. Acceptable values are 'enforced' or 'inherited'."
+ 
+}
